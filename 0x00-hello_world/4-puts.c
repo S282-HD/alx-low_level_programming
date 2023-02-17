2298d57be8910fcdd4ed45b6ba8fcbe37de17eb6
S282-HD <shahd.abdelhamied@gmail.com>
2023-02-16 17:13:14 -0800

    put4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * main - entry point
- * Description: prints "\"Programming is like building a multilingual puzzle"
- * Return: 0
+ * main -A program that prints a line with puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {

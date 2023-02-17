cadcdccada74eff0ce3dc893466eb0872b80b0a0
S282-HD <shahd.abdelhamied@gmail.com>
2023-02-16 16:31:47 -0800

    puts4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stadio.h>
+/**
+*main - entry point
+*Description: prints "\"programming is like building a multilngual puzzle"
+*Return:0
+*/
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}

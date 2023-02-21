836efc012f6b8d427a2d00400a05c23ccbac41a7
S282-HD <shahd.abdelhamied@gmail.com>
2023-02-21 13:53:36 -0800

    bbg

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *ch = "_putchar\n";
+int i = 0;
+while (ch[i])
+{
+	_putchar(ch[i]);
+	i++;
+}
+return (0);
+}
